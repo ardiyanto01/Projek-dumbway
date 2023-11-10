30fcf60ed677765d6828ba3328a8449ec265280c
Steve ardiyanton <steveardiyanto3@gmail.comn>
2023-11-10 23:59:05 +0700

    halo

---
+++ day3/contact.js
@@ -0,0 +1,21 @@
+// let name = "hellow"
+// console.log(name)
+// console.log(name + "surya")
+// var name ="Novri"
+// var age = "30"
+// console.log(`selamat datang wahai : ${name} , ${age} `)
+
+// operator ( + - * / %)
+// let bilangan1 = 105
+// let bilangan2 = 50
+// console.log(bilangan1 % bilangan2)
+
+// condition
+const nilai = 19
+if(nilai > 80) {
+    console.log("nilai A")
+} else if (nilai < 20){
+    console.log("nilai C")
+} else {
+    console.log("nilai g ada")
+}
